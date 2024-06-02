a62a24f3b42acaa27467de345ea79b3dd849e8f2
knight <karvin_jimenez@hotmail.com>
2024-06-02 12:13:43 -0400

    extra changes

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("hello world\n");
+
+    //To print and round a float to 3 decimals
+    printf("this is a float %2.3f\n", 3.1457f);
+
+    // print a float without round  
+    printf("this is a float %f\n", 3.1457f);
+    printf("this is a number %d\n", 3);
+    printf("this is a string %s\n", "hola");  
+    printf("this is a character %c\n", 'j');
+}
